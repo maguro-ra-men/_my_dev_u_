f1f1ac2ea8268326fd8a7ea5ad187dac1c4cd5cc
maguro-ra-men <pc.tiredoftax@gmail.com>
2022-01-13 10:43:28 +0900

    Create urls.py

---
+++ python/code/dev/mysite/polls/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from .views import IndexView, AboutView
+
+urlpatterns = [
+    path('', IndexView.as_view(), name="index"),
+    path('aboutus/', AboutView.as_view(), name="about"),
+    path('df/', AboutView.as_view(), name="df"),
+]
